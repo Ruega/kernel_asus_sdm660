efd8738cd8e84fed7516bc2d27331ec9bdb524af
Andrei F <luxneb@gmail.com>
2018-05-25 18:53:39 +0200

    sched/fair: Port Migration Hysteresis Filter from HiSilicon EAS
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>
    Signed-off-by: ben863 <r.budhiono@gmail.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -123,3 +123,5 @@ SCHED_FEAT(MIN_CAPACITY_CAPPING, true)
  * If disabled, boosts will only bias tasks to higher-capacity CPUs.
  */
 SCHED_FEAT(SCHEDTUNE_BOOST_UTIL, false)
+
+SCHED_FEAT(HISI_FILTER, true)
